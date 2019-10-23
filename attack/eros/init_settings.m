937c2ac645fdf4861edbad69f455872f5010bdda
Eros <>
2019-10-23 13:35:36 +0200

    Added AttackConfig and example if its use in init_settings.m

--- attack/eros/init_settings.m
+++ attack/eros/init_settings.m
@@ -1,3 +1,11 @@
+%ATTACK SETTINGS
+attack_config = AttackConfig; %Create an instance
+attack_config.lazy = false; %If true then stop attacking ASA a succesful attack is delivered
+attack_config.filters = [ %List of filters with params to be applied
+FilterConfiguration(FilterEnum.BLURRING, 2, 0, 0, 0, 0, 0)
+FilterConfiguration(FilterEnum.SHARPENING, 3, 0, 0, 0, 0, 1)
+FilterConfiguration(FilterEnum.AWGN, .000001, 100, 0, 0, 0, 0)
+];
 %GENERAL SETTINGS
 groupname = 'unemployed'; %Our group name
 do_export = 1; %Enable/disable log export
