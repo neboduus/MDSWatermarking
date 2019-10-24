c324df87ecb90c231a464c72a8caf43119989b6b
Eros <>
2019-10-24 12:00:29 +0200

    Display and export the best round

--- attack/eros/run_attack.m
+++ attack/eros/run_attack.m
@@ -1,18 +1,20 @@
-function [attack_log] = run_attack(agroupname, I_name, wI_name)
+function [best_log] = run_attack(agroupname, I_name, wI_name)
   init_settings %load initial settings
   fprintf('>ATTACK image %s\n',I_name);
   I_name=strcat('img/nowatermark/',I_name); %Set image path
   wI_name=strrep(I_name,'nowatermark/',strcat(agroupname,'_')); %Set image path
-  attack_outcome = 0; %Holds the outcome of the current attack
-  attack_WPSNR = 100; %Holds the current WPSNR
-  attack_log = {}; %Holds the attack log
   minWPSNR = 35; %Minimum WPSNR accepted to break detection
   aI_name = strrep(wI_name,'img/',strcat('img/',groupname,'_')); %Set attacked image path
-
+  bestround = -1; %Holds the id of the best round (succesful attack with highest WPSNR)
+  bestWPSNR = -1; %Holds the WPSNR of the best round
+  best_log = {}; %Holds the attack log of the best round
   i = 1;
   stop = false;
   while stop == false
     fprintf('Start round %i\n',i);
+    attack_outcome = 0; %Holds the outcome of the current attack
+    attack_WPSNR = 100; %Holds the current WPSNR
+    attack_log = {}; %Reset the attack log
     imwrite(imread(wI_name), aI_name); %Copy original watermarked image to attack
     mapping = mappings(i,:); %Consider the ith attack mapping
     j = 1; %Consider the jth filter
@@ -29,10 +31,19 @@ function [attack_log] = run_attack(agroupname, I_name, wI_name)
       attack_outcome = d_outcome == 0 && d_WPSNR >= minWPSNR;
       attack_WPSNR = d_WPSNR;
       %DETECTION CALL ENDS HERE
+
+      if(attack_outcome == 1 && attack_WPSNR>bestWPSNR)
+        %Keep track of the best round until now
+        bestround = i;
+        bestWPSNR = attack_WPSNR;
+        best_log = attack_log;
+      end
+
       fprintf('   %s (%s) -> attack_outcome=%d, WPSNR=%f\n',filter_config.filter.name, filter_config.parameters, attack_outcome, attack_WPSNR);
       j = j + 1;
     end
     fprintf('End round %i: attack_outcome=%d, WPSNR=%f\n',i, attack_outcome, attack_WPSNR);
+
     i = i + 1;
     stop = i > size(mappings, 1); %Stop when all mapping have been executed
     if attack_config.lazy
@@ -41,12 +52,24 @@ function [attack_log] = run_attack(agroupname, I_name, wI_name)
     end
   end
 
+  if bestround == -1
+    %Failed attack, return empty log
+    fprintf('========\nNo successful round');
+    best_log = [];
+  else
+    attacks = sprintf('%s',cell2mat(best_log));
+    fprintf('========\nRound %i was the best with WPSNR=%f: %s', bestround, bestWPSNR, attacks);
+    if do_export == 1
+      export(aI_name, agroupname, attack_WPSNR, best_log, groupname);
+    end
+  end
+
   %Failed attack, return empty log
   if attack_outcome == 0
-    attack_log = [];
+    best_log = [];
     %If export is enabled, save the csv with the logged attacks
   elseif do_export == 1
-    export(aI_name, agroupname, attack_WPSNR, attack_log, groupname);
+    export(aI_name, agroupname, bestWPSNR, best_log, groupname);
   end
 
 end
